d1e08aeb58b6f7913de2e4996f465ee184d04788
Suniti <suniti0804@gmail.com>
2022-02-24 19:25:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/project8/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.project8
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.example.project8.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mAdapter: NewsListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        binding.recyclerView.adapter = mAdapter
+    }
+
+    private fun fetchData(){
+        val url = "https://newsdata.io/api/1/news?apikey=pub_490283436ab00426883e3bb0aea3082f5261&language=en"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                val newsJsonArray = it.getJSONArray("results")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("creator"),
+                        newsJsonObject.getString("link"),
+                        newsJsonObject.getString("image_url")
+                    )
+                    if(news.image_url!="null")
+                       newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+            },
+            {
+
+            }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    override fun onItemClicked(item: News) {
+        val builder =  CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.link))
+    }
+}
---
+++ app/src/main/java/com/example/project8/News.kt
@@ -0,0 +1,8 @@
+package com.example.project8
+
+data class News(
+    val title: String,
+    val creator: String,
+    val link: String,
+    val image_url: String?
+)
